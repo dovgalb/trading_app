ef280025f81c3766ef610cdd0d9517403b0312dc
dovgalb <bogdandovgal1208@gmail.com>
2023-04-29 17:45:19 +0300

    lesson3

--- lesson_2/main.py
+++ lesson_2/main.py
@@ -4,3 +4,39 @@ app = FastAPI(
     title="Trading App"
 )
 
+fake_users = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+
+fake_users2 = [
+    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 2, "role": "investor", "name": "John"},
+    {"id": 3, "role": "trader", "name": "Matt"},
+]
+
+
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC", "side": "buy", "price": 123, "amount": 2.12},
+    {"id": 2, "user_id": 1, "currency": "BTC", "side": "sell", "price": 124, "amount": 2.12},
+    {"id": 3, "user_id": 2, "currency": "BTC", "side": "buy", "price": 121, "amount": 2.2},
+]
+
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return [user for user in fake_users if user.get('id') == user_id]
+
+
+@app.get('/trades')
+def get_trades(limit: int = 2, offset: int = 1):
+    return fake_trades[offset:][:limit]
+
+
+@app.post('/users/{user_id}')
+def change_user_name(user_id: int, new_name: str):
+    current_user = list(filter(lambda user: user.get('id') == user_id, fake_users2))[0]
+    current_user['name'] = new_name
+    return {"status": 200, "data": current_user}
