86c68a1bd538086a3a3a01ef5231fbb3c1e14d9c
dovgalb <bogdandovgal1208@gmail.com>
2023-05-14 18:15:14 +0300

    auth

--- lesson_5/models/models.py
+++ lesson_5/models/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, Boolean
 
 metadata = MetaData()
 
@@ -25,8 +25,8 @@ metadata = MetaData()
 # )
 
 
-roles = Table(
-    "roles",
+role = Table(
+    "role",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
@@ -34,14 +34,16 @@ roles = Table(
 )
 
 
-users = Table(
-    "users",
+user = Table(
+    "user",
     metadata,
     Column("id", Integer, primary_key=True),
     Column('email', String, nullable=False),
     Column('username', String, nullable=False),
-    Column('password', String, nullable=False),
     Column('registered_at', TIMESTAMP, default=datetime.utcnow),
-    Column("role_id", Integer, ForeignKey("roles.id")),
-
+    Column("role_id", Integer, ForeignKey(role.c.id)),
+    Column('hashed_password', String, nullable=False),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
